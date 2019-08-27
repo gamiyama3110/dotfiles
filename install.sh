8a9bd8082aab82b78e9acdab77d2d1f49e78b040
t_yamagami <t_yamagami@fancs.com>
2019-08-27 12:38:03 +0900

    add setup brew

--- install.sh
+++ install.sh
@@ -7,7 +7,18 @@ for f in .??*
 do
   [[ "$f" == ".git" ]] && continue
   [[ "$f" == ".DS_Store" ]] && continue
+  [[ "$f" == ".profile" ]] && continue
 
   # echo $THIS_DIR/"$f"
   ln -snfv $THIS_DIR/"$f" ~/
 done
+
+## setup brew
+if !(type "brew" > /dev/null 2>&1);
+  then $THIS_DIR/etc/setup/bash.sh
+fi
+brew bundle cleanup --global
+
+
+echo "Install Finished." | cowsay
+# END
